e61b02b2c33cbb8a8387105c8c3f73a4f4225933
Aridsondez <schoolaridsondez@gmail.com>
2025-01-28 10:54:22 -0500

    created rouch models

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class EnergyData(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
+    energy_generated = models.FloatField()
+    energy_consumed = models.FloatField()
+
+class SolarPanel(models.Model):
+    panel_id = models.CharField(max_length= 50, unique=True)
+    last_maintenance_date = models.DateField()
+    efficiency = models.FloatField()
+    current_output = models.FloatField()
+
+    
