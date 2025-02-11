445297d805764e8b1de7a1de6045e9f5de4df64a
Aridsondez <schoolaridsondez@gmail.com>
2025-02-11 11:39:36 -0500

    new title

--- frontend/Solar-Panel/src/app/app.component.spec.ts
+++ frontend/Solar-Panel/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Solar-Panel' title`, () => {
+  it(`should have the 'solar-panel' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Solar-Panel');
+    expect(app.title).toEqual('solar-panel');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, Solar-Panel');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, solar-panel');
   });
 });
