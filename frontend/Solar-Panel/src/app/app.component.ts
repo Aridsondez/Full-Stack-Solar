2282865cdbbf8fed48a3fbfd736af02417e42d53
Aridsondez <schoolaridsondez@gmail.com>
2025-03-01 23:49:11 -0500

    created services

--- frontend/Solar-Panel/src/app/app.component.ts
+++ frontend/Solar-Panel/src/app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import {WelcomeComponent} from './welcome/welcome.component';
+import { ServicesComponent } from './services/services.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    HeaderComponent,
+    WelcomeComponent,
+    ServicesComponentcd 
+
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+
+  counter: number = 0
   title = 'solar-panel';
+
+  add(){
+    this.counter++;
+  }
 }
--- frontend/Solar-Panel/src/app/welcome/welcome.component.ts
+++ frontend/Solar-Panel/src/app/welcome/welcome.component.ts
@@ -8,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class WelcomeComponent {
 
+
+  welcomeStatement: string = "Welcome to ShellShock ⚡"
+  shellShockDescription : string = "A Solar Powered Montering App"
+
 }
